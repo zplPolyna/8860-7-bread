b9458ec2ed43328d658b941b52c5cf8a2e60b6c2
zplPolyna <116415623+zplPolyna@users.noreply.github.com>
2022-10-22 11:59:05 +0800

    Add files via upload

---
+++ soundcheck-main/app/app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template, request
+import numpy as np
+import pandas as pd
+from joblib import load
+import os
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def hello_world():
+    request_type_str = request.method
+    if request_type_str == 'GET':
+        return render_template('index.html')
+    else:
+        return render_template('index.html')
